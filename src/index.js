a4516fca5c27afe75172db72fdcaf914315fa53a
Ranjith1026 <83263858+Ranjith1026@users.noreply.github.com>
2021-06-15 06:56:27 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import Header from '../src/components/header';
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Header />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
